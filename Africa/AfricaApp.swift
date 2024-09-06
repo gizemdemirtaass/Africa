e27fab2835309a9be6c87579872842f85da63338
gizemdemirtaass <gizzemdemirtas@gmail.com>
2024-09-06 17:54:53 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by gizem demirtas on 6.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
